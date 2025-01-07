7721508592fe6c735a4ca5556b8e0b8dcd8d6621
AgustinZavalaA <1930120@upv.edu.mx>
2025-01-07 02:06:45 -0600

    added thread to delete completed tasks

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import threading
 from fastapi import FastAPI
 import uvicorn
+import time
 
 import auth
-from database import engine
+from database import LocalSession, engine
+from models import Task
 import models
 import tasks
 
@@ -11,6 +14,25 @@ app.include_router(auth.router)
 app.include_router(tasks.router)
 # models.Base.metadata.drop_all(engine) # only uncomment this if you dont care about the data in the db or if encounter a db issue
 models.Base.metadata.create_all(bind=engine)
+
+
+def delete_complete_tasks() -> None:
+    while True:
+        db = LocalSession()
+        try:
+            # print("DELETING COMPLETED TASKS")
+
+            db.query(Task).filter(Task.completed).delete()
+            db.commit()
+        finally:
+            db.close()
+
+        # time.sleep(30) # used for debugging
+        time.sleep(10 * 60)  # 10 minutes * 60 seconds
+
+
+delete_tasks_thread = threading.Thread(target=delete_complete_tasks, daemon=True)
+delete_tasks_thread.start()
 
 
 def main() -> None:
